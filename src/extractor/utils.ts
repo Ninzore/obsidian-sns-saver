c91dd4a8def7efe22b75363b8a96593d474fab0d
Ninzore <33434617+Ninzore@users.noreply.github.com>
2024-01-05 12:28:31 +0000

    Feat: new request function

--- src/extractor/utils.ts
+++ src/extractor/utils.ts
@@ -1,10 +1,56 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import axios from 'axios';
+import { requestUrl } from 'obsidian';
 
 const MAX_SIZE = 100 * 1024 * 1024;
 const ACCEPT_TYPE = ['jpg', 'jpeg', 'png', 'gif', 'webp', 'mp4', 'webm'];
 const UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36';
+
+interface RequestOption<T extends 'GET' | 'POST' | 'PUT' | 'DELETE'> {
+    method: T;
+    body?: T extends 'POST' | 'PUT' | 'DELETE' ? string | ArrayBuffer : never;
+    params?: { [key: string]: any; };
+    headers?: {
+        'User-Agent'?: string;
+        'Accept'?: string;
+        'Cookies'?: string;
+        'Authorization'?: string;
+    };
+    responseType?: '' | 'text' | 'json' | 'document' | 'blob' | 'arraybuffer' | 'stream';
+}
+
+export async function request(
+    url: string,
+    requestOption: RequestOption<'GET' | 'POST' | 'PUT' | 'DELETE'>
+): Promise<any> {
+    const params = requestOption.params
+        ? new URLSearchParams(requestOption.params).toString()
+        : '';
+
+    return requestUrl({
+        url: `${url}?${params}`,
+        method: requestOption.method,
+        body: requestOption.body,
+        headers: requestOption.headers
+    }).then(res => {
+        if (requestOption.responseType === 'json') {
+            return res.json();
+        }
+        else if (requestOption.responseType === 'stream') {
+            return res.arrayBuffer;
+        }
+        else {
+            try {
+                const resJson = JSON.parse(res.text);
+                return resJson;
+            }
+            catch (e) {
+                return res.text;
+            }
+        }
+    });
+}
 
 export async function download(url: string, filepath: string): Promise<fs.PathLike> {
     return new Promise((resolve, reject) => {
