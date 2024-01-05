ca20738b7a7b7836a34f5b531df045aefbc4eaa7
Ninzore <33434617+Ninzore@users.noreply.github.com>
2024-01-05 14:19:54 +0000

    Feat: new class Formatter for gen Markdown

---
+++ src/formatter.ts
@@ -0,0 +1,35 @@
+import { SnsContent, MarkdownConent } from "./types/format";
+
+export class Formatter {
+    async format(snsContent: SnsContent): Promise<MarkdownConent> {
+        try {
+            const content = snsContent.content.map(content => {
+                switch (content.type) {
+                    case 'text':
+                        return content.text;
+                    case 'image':
+                        return `![](${content.url})`;
+                    case 'video':
+                        return `![](${content.url})`;
+                    default:
+                        return '';
+                }
+            }).join('\n');
+
+            return {
+                title: snsContent.title,
+                author: snsContent.author,
+                article: '',
+                date: snsContent.date.toISOString(),
+                content: content,
+                source: snsContent.source,
+                length: content.length,
+            };
+
+        }
+        catch (e) {
+            console.error(e);
+            throw e;
+        }
+    }
+}
